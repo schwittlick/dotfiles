2536cc35aef1819a98c029cf424a535f9920998c
Marcel Schwittlick <info@schwittlick.net>
2026-02-02 22:51:25 +0100

    pacman setup

--- arch/setup.sh
+++ arch/setup.sh
@@ -1,3 +1,6 @@
+sudo sed -i 's/#Color/Color/' /etc/pacman.conf
+sudo pacman --needed --noconfirm -Sy git openssh base-devel
+
 # install yay if not installed
 if ! command -v yay; then
 	# install yay
